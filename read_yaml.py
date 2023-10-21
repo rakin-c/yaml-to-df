ba691d969e2e831693d279ab56f480aa9eac2cbe
rakin-c <147671815+rakin-c@users.noreply.github.com>
2023-10-21 03:57:56 +0100

    Updates read_yaml.py

--- read_yaml.py
+++ read_yaml.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 import yaml
 
-def yaml_to_json(file: str):
+def yaml_to_df(file: str):
     with open(file, "r") as f:
         json_file = json.dumps(yaml.safe_load(f))
     table = pd.read_json(json_file)
     return table
 
 if __name__ == "__main__":
-	print(yaml_to_json("yaml_example.yaml"))
+	print(yaml_to_df("yaml_example.yaml"))
