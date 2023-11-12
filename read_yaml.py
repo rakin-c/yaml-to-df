c7a94b2af05d72678b74fda801601a249e8b5f55
rakin-c <147671815+rakin-c@users.noreply.github.com>
2023-11-12 14:04:55 +0000

    Changes function and variable names for consistency

--- read_yaml.py
+++ read_yaml.py
@@ -2,11 +2,8 @@ import pandas as pd
 import json
 import yaml
 
-def yaml_to_df(file: str):
+def read_yaml(file: str):
     with open(file, "r") as f:
         json_file = json.dumps(yaml.safe_load(f))
-    table = pd.read_json(json_file)
-    return table
-
-if __name__ == "__main__":
-	print(yaml_to_df("yaml_example.yaml"))
+    df = pd.read_json(json_file)
+    return df
